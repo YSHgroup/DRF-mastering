6c784cd7eb8d19a7400b080d6b45ad7712f4c91c
MagicSky <david.dev100@outlook.com>
2024-03-11 20:28:02 -0400

    Install the django-cors-headers and add it to installed apps and middleware

--- drf_crud/drf_crud/settings.py
+++ drf_crud/drf_crud/settings.py
@@ -37,17 +37,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_crud',
+    'corsheaders' # CORS
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # CORS 
+    'corsheaders.middleware.Corsmiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8001',
+)
 
 ROOT_URLCONF = 'drf_crud.urls'
 
@@ -61,9 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'rest_framework',
-                'drf_crud'
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },
