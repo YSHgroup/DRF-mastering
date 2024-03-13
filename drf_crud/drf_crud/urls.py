846749e211eceb27fd103c063df25b4a89296c29
MagicSky <david.dev100@outlook.com>
2024-03-12 23:22:08 -0400

    Update model and process migration

--- drf_crud/drf_crud/urls.py
+++ drf_crud/drf_crud/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('simple_crud.urls')),
+    path('api/', include('simple_crud.urls', namespace='tutorial_list')),
     path('api-auth/', include('rest_framework.urls'))
 ]
