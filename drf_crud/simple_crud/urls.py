b651be7064b88bc42286d81756eb51ec96fd4521
MagicSky <david.dev100@outlook.com>
2024-03-12 23:22:43 -0400

    Add the urls in simple_crud app

--- drf_crud/simple_crud/urls.py
+++ drf_crud/simple_crud/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+app_name = 'simple_crud'
+"""
+ Why do we use the app name?
+ - django.core.exceptions.ImproperlyConfigured: Specifying a namespace in include() without providing an app_name is not supported. Set 
+ the app_name attribute in the included module, or pass a 2-tuple containing the list of patterns and app_name instead.
+"""
 
 urlpatterns = [
-    path('', views, name="")
+    path('tutorials', views.tutorial_list, name='tutorials'),
+    re_path(r'^tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail, name='tutorial_detail'),
+    path('tutorials/published', views.tutorial_list_published, name='tutorials_published')
 ]
