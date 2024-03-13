f57f33b3622b9bc38e283d98f04e4ccfe7134f5a
MagicSky <david.dev100@outlook.com>
2024-03-12 23:20:56 -0400

    Add cors headers

--- drf_crud/drf_crud/settings.py
+++ drf_crud/drf_crud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'simple_crud',
+    'simple_crud.apps.SimpleCrudConfig',
     'corsheaders' # CORS
 ]
 
@@ -46,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     # CORS 
-    'corsheaders.middleware.Corsmiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,9 +55,21 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+ALLOWED_HOSTS = [
+    'example-us.vercel.app',
+    'localhost',
+    '*'
+]
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8001',
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8000',
+#     'http://localhost:8001',
+#     'http://127.0.0.1:8001',
+# ]
+CORS_URLS_REGEX = r'^(?:http://)?(?:localhost:\d{4}|127\.0\.0\.1:\d{4})/$'
+
+CORS_ALLOW_HEADERS = (
+    *default_headers,
 )
 
 ROOT_URLCONF = 'drf_crud.urls'
